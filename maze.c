70ba372df8f2ed9031357a063914dfa46fc42819
davidchoi1007 <davidchoi107@naver.com>
2022-09-06 22:07:46 +0900

    1st commit

---
+++ maze.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+//void select_level()
+//{
+
+//}
+
+int main()
+{
+    printf("미로 찾기게임\n 시작하려면 enter키를 입력하세요.");
+
+    char enter;
+    enter = getchar();
+    
+    if(enter == '\n'){
+        //select_level();
+        printf("hello");
+    }
+
+    return 0;
+}
