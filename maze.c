28ff09bb3e04ad9bdfa3a4979df4cd931dce3236
sw107 <davidchoi107@naver.com>
2022-09-13 22:01:26 +0900

    create maze.h file

--- maze.c
+++ maze.c
@@ -1,35 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<windows.h>
-
-// 상하좌우 상수값
-#define up 72
-#define down 80
-#define left 75
-#define right 77
-
-// 글자 색상 변경
-#define color(A) SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), A);
-
-// 커서 설정 (1 == on / 0 == off)
-void cursor(int n)
-{
-    HANDLE hConsole;
-    CONSOLE_CURSOR_INFO ConsoleCursor;
-
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    ConsoleCursor.bVisible = n;
-    ConsoleCursor.dwSize = 1;
-
-    SetConsoleCursorInfo(hConsole, &ConsoleCursor);
-}
-
-// 글자 출력 위치 설정
-void Gotxy(int x, int y)
-{
-    COORD Pos = { y - 1,x - 1 };
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-}
+#include "maze.h"
 
 //단계 선택 함수
 void select_level()
---
+++ maze.h
@@ -0,0 +1,34 @@
+#ifndef MAZE_H
+# define MAZE_H
+# include<windows.h>
+
+// 상하좌우 상수값
+# define up 72
+# define down 80
+# define left 75
+# define right 77
+
+// 글자 색상 변경
+# define color(A) SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), A);
+
+// 커서 설정 (1 == on / 0 == off)
+void cursor(int n)
+{
+    HANDLE hConsole;
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    ConsoleCursor.bVisible = n;
+    ConsoleCursor.dwSize = 1;
+
+    SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
+
+// 글자 출력 위치 설정
+void Gotxy(int x, int y)
+{
+    COORD Pos = { y - 1,x - 1 };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+#endif 
